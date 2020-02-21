edd2f09465c2fe8cbc0f8d37d0108a56a8a8ce20
CarlosGo1994 <charls21ronald@gmail.com>
2020-02-21 00:42:00 -0600

    Basic example how to do a for loop and use the debugger keyword

---
+++ script.js
@@ -0,0 +1,13 @@
+'use strict'
+
+// For loop
+// Loop is a control structure which it is repeated several times
+
+let numbers = 100;
+
+for (let i = 0; i <= numbers; i++)
+{
+    console.log("Let's go to the number: "+ i);
+
+    debugger;// this sentence is useful for we can monitoring the program course
+}
